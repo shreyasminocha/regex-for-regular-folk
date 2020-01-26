76edab42c3347fba7ca1864ed92695f3cc00ad52
Shreyas Minocha <sm-git@riseup.net>
2020-01-26 14:14:16 +0530

    Remove old export script

--- export.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env/bash
-
-pandoc -t gfm --wrap none -o dist/book.md chapters/*.md
-pandoc meta.yml -o dist/book.epub chapters/*.md
-pandoc -f markdown-implicit_figures -o dist/book.pdf chapters/*.md --metadata-file meta.yml --dpi 300
