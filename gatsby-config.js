794c231d86b375b80769633d2ed5ede53820af21
Shreyas Minocha <sm-git@riseup.net>
2020-03-23 18:28:56 +0530

    Add autolink-headers plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,17 @@ module.exports = {
         {
             resolve: "gatsby-plugin-mdx",
             options: {
-                gatsbyRemarkPlugins: [{ resolve: "gatsby-remark-smartypants" }]
+                gatsbyRemarkPlugins: [
+                    {
+                        resolve: "gatsby-remark-smartypants"
+                    },
+                    {
+                        resolve: "gatsby-remark-autolink-headers",
+                        options: {
+                            icon: false
+                        }
+                    }
+                ]
             }
         }
     ]
