7a0b0a69166bb87e02e4341e838da4b73617e158
Shreyas Minocha <sm-git@riseup.net>
2020-04-24 00:27:24 +0530

    Refactor Example component

--- src/components/example/index.js
+++ src/components/example/index.js
@@ -4,74 +4,38 @@ import PropTypes from "prop-types";
 import "./index.css";
 
 const Example = (props) => {
-    const { regex, children } = props;
+    const {regex, children} = props;
+    const testStrings = children.map((str) => str.props.children || "");
 
-    const testCases = children.map(str => {
-        const string = str.props.children || "";
-        const matches = string.matchAll(regex);
-
-        const matchesListItems = [];
-        let runningMatchLength = 0;
-
-        for (const match of matches) {
-            const [str] = match;
-            const { index } = match;
-            const left = index - runningMatchLength;
-
-            matchesListItems.push(
-                <li style={{ width: `${str.length}ch`, left: `${left}ch` }} key={str}>
-                    {str}
-                </li>
-            );
-
-            runningMatchLength += str.length;
-        }
-
-        const numMatches = matchesListItems.length;
-        const anyMatches = numMatches !== 0;
-        const indicatorColour = anyMatches ? "#73fa79" : "#ff7e79";
-
-        return (
-            <li>
-                <span
-                    className="pass-fail"
-                    style={{ backgroundColor: indicatorColour }}
-                >
-                    {numMatches} match{numMatches !== 1 ? "es" : ""}
-                </span>
-
-                {string}
-                <ol>{matchesListItems}</ol>
-            </li>
-        );
-    });
-
-    const noop = ({children}) => <>{children}</>;
-    const code = ({children}) => <code className="regex-name">{children}</code>;
-
-    const playGroundText = children.map(
-        str => str.props.children || ""
-    ).join("\n");
-
+    const multiLineRegex = `${regex.toString()}m`; // works fine even if `regex` is multiline
+    const playGroundText = testStrings.join("\n");
     const playGroundUrl = `
         https://regexr.com/?expression=${
-            encodeURIComponent(`${regex.toString()}m`)
+            encodeURIComponent(multiLineRegex)
         }&text=${
             encodeURIComponent(playGroundText)
         }
     `.trim();
 
+    const noop = ({children}) => <>{children}</>;
+    const code = ({children}) => <code className="regex-name">{children}</code>;
+
+    // Prism language is "javascript" so that we have access to the
+    // `regex-delimiter` and `regex-flags` tokens as well
     return (
         <figure className="example">
             <Prism language="javascript" PreTag={noop} CodeTag={code}>
                 {regex.toString()}
             </Prism>
 
-            <a href={playGroundUrl} target="_blank" style={{marginLeft: "1em"}}>
+            <a href={playGroundUrl} className="regexr-link"
+            target="_blank" rel="noopener noreferrer">
                 [RegExr]
             </a>
 
-            <ul>{testCases}</ul>
+            <ul>
+                {testCases(regex, testStrings)}
+            </ul>
         </figure>
     );
 };
@@ -85,5 +49,55 @@ Example.defaultProps = {
     children: [],
     flags: ""
 };
+
+const testCases = (regex, strings) => (
+    strings.map((string) => {
+        const matchesListItems = matchesList(regex, string);
+
+        const numMatches = matchesListItems.length;
+        const didMatch = numMatches !== 0;
+
+        const indicator = {
+            backgroundColor: didMatch ? "#73fa79" : "#ff7e79"
+        };
+
+        return (
+            <li>
+                <span className="indicator" style={indicator}>
+                    {numMatches} match{numMatches !== 1 ? "es" : ""}
+                </span>
+
+                {string}
+
+                <ol>{matchesListItems}</ol>
+            </li>
+        );
+    })
+);
+
+function matchesList(regex, string) {
+    const matches = string.matchAll(regex);
+
+    const items = [];
+    let runningMatchLength = 0;
+
+    for (const match of matches) {
+        const [str] = match;
+        const {index} = match;
+
+        const matchStyling = {
+            width: `${str.length}ch`,
+            left: `${index - runningMatchLength}ch`
+        };
+
+        items.push(
+            <li style={matchStyling} key={str}>{str}</li>
+        );
+
+        runningMatchLength += str.length;
+    }
+
+    return items;
+}
 
 export default Example;
