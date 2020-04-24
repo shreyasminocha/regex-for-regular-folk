514f065a01d4bd911cd8acf30335cc6e08f531e4
Shreyas Minocha <sm-git@riseup.net>
2020-04-24 23:19:26 +0530

    Add next and previous links

--- src/layouts/chapter.js
+++ src/layouts/chapter.js
@@ -1,23 +1,59 @@
 import React from "react";
 import { MDXProvider } from "@mdx-js/react";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 import { graphql } from "gatsby";
 import Example from "../components/example";
 import Note from "../components/note";
 import Warning from "../components/warning";
-import { MDXRenderer } from "gatsby-plugin-mdx";
+import * as meta from "../../meta.json";
 
 const shortcodes = { Example, Note, Warning };
 
 export default (props) => {
     const { mdx } = props.data;
+    const { title } = mdx.frontmatter;
+
+    const slugifiedTitle = mdx.fields.slug.split("/")[1];
+
+    const prevIndex = meta.chapters.indexOf(slugifiedTitle) - 1;
+    const nextIndex = meta.chapters.indexOf(slugifiedTitle) + 1;
+
+    const prev = prevIndex >= 0 ? meta.chapters[prevIndex] : undefined;
+    const next =
+        nextIndex < meta.chapters.length ? meta.chapters[nextIndex] : undefined;
+
+    let prevItem = <></>;
+    let nextItem = <></>;
+
+    if (prev !== undefined) {
+        prevItem = (
+            <li>
+                <a href={`/chapters/${prev}`}>Previous</a>
+            </li>
+        );
+    }
+
+    if (next !== undefined) {
+        nextItem = (
+            <li>
+                <a href={`/chapters/${next}`}>Next</a>
+            </li>
+        );
+    }
 
     return (
         <main>
-            <h1>{mdx.frontmatter.title}</h1>
+            <h1>{title}</h1>
 
             <MDXProvider components={shortcodes}>
                 <MDXRenderer>{mdx.body}</MDXRenderer>
             </MDXProvider>
+
+            <nav>
+                <ul>
+                    {prevItem} {nextItem}
+                </ul>
+            </nav>
         </main>
     );
 };
@@ -30,6 +66,9 @@ export const query = graphql`
             }
             body
             id
+            fields {
+                slug
+            }
         }
     }
 `;
