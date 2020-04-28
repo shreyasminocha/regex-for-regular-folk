a8c74daa8bd3ec4c37818436c8d07c4cc976d2e5
Shreyas Minocha <sm-git@riseup.net>
2020-04-29 00:44:36 +0530

    Add custom 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,19 @@
+import React from "react";
+import SEO from "../components/seo";
+
+const FourZeroFour = () => (
+    <>
+        <SEO title="Page Not Found" />
+
+        <h1>Page Not Found</h1>
+
+        <pre>
+            <code>$ tree | grep -E "$path"</code>
+            <code>🚫 $</code>
+        </pre>
+
+        <a href="/">🏠 Home?</a>
+    </>
+);
+
+export default FourZeroFour;
